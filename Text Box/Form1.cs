df10f0aa192fda3e1817e67fea5fbe215a69fe25
rc0032716 <rc0032716@CRO-24-01>
2019-05-14 09:45:12 +0100

    Add project files.

---
+++ Text Box/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int i = 3;
+            richTextBox1.AppendText(" " + i);
+            double pi = 3.14159265;
+            richTextBox1.AppendText(pi.ToString());
+            richTextBox1.AppendText(3.ToString());
+        }
+    }
+}
